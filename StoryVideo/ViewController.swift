cb2644859f6d8dd63ea4436ed970fb1d7edc2dec
Dilip <Dilipkumar@pepul.com>
2022-09-30 17:10:28 +0530

    Initial Commit

---
+++ StoryVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryVideo
+//
+//  Created by Dilip Kumar s on 30/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
